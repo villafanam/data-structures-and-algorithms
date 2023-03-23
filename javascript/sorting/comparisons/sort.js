93e01a595460d28ebcd93f922efdde574f8510ce
Marco <pastry07@gmail.com>
2023-03-22 20:58:51 -0700

    CC28 complete

--- javascript/sorting/comparisons/sort.js
+++ javascript/sorting/comparisons/sort.js
@@ -4,22 +4,58 @@
 
 // need to write the "callback" compareYear
 function sortYear(arr) {
-  arr.sort(compareYear);
+  return arr.sort(compareYear);
 }
 
 
-function compareYear() {
+function compareYear(a, b) {
   //do the thing
+  return b.year - a.year;
 }
 
 function sortTitle(arr) {
-  arr.sort(compareTitle);
+  return arr.sort(compareTitle);
+
 }
 
 
-function compareTitle() {
+function compareTitle(a, b) {
   //do the thing
+  let a2 = '';
+  let b2 = '';
+
+  if (a.title.includes('A ')) {
+    a2 = a.title.replace('A ', '');
+  }
+  else if (a.title.includes('An ')) {
+    a2 = a.title.replace('An ', '');
+  }
+  else if (a.title.includes('The ')) {
+    a2 = a.title.replace('The ', '');
+  }
+  else {
+    a2 = a.title;
+  }
+
+  if (b.title.includes('A ')) {
+    b2 = b.title.replace('A ', '');
+  }
+  else if (b.title.includes('An ')) {
+    b2 = b.title.replace('An ', '');
+  }
+  else if (b.title.includes('The ')) {
+    b2 = b.title.replace('The ', '');
+  }
+  else {
+    b2 = b.title;
+  }
+
+  if (a2 > b2) { return 1; }
+
+  if (a2 < b2) { return -1; }
+
+  return 0;
 }
 
 
-module.exports = {sortYear, compareYear, sortTitle, compareTitle};
+module.exports = { sortYear, compareYear, sortTitle, compareTitle };
--- javascript/sorting/comparisons/sort.test.js
+++ javascript/sorting/comparisons/sort.test.js
@@ -1,38 +1,38 @@
 'use strict';
 
-const { Movies } = require("./movies");
-const { sortTitle, sortYear } = require("./sort");
+const { Movies } = require('./movies');
+const { sortTitle, sortYear } = require('./sort');
 
-describe("Sorters", () => {
-  it("can sort movies by year", () => {
+describe('Sorters', () => {
+  it('can sort movies by year', () => {
     const movies = sortYear(Movies);
     expect(movies.map((m) => m.title)).toEqual([
-      "The Intouchables",
-      "Valkyrie",
-      "Ratatouille",
-      "Stardust",
-      "City of God",
-      "Memento",
-      "The Shawshank Redemption",
-      "Beetlejuice",
-      "Crocodile Dundee",
-      "The Cotton Club",
+      'The Intouchables',
+      'Valkyrie',
+      'Ratatouille',
+      'Stardust',
+      'City of God',
+      'Memento',
+      'The Shawshank Redemption',
+      'Beetlejuice',
+      'Crocodile Dundee',
+      'The Cotton Club',
     ]);
   });
 
-  it("can sort movies by title", () => {
+  it('can sort movies by title', () => {
     const movies = sortTitle(Movies);
     expect(movies.map((m) => m.title)).toEqual([
-      "Beetlejuice",
-      "City of God",
-      "The Cotton Club",
-      "Crocodile Dundee",
-      "The Intouchables",
-      "Memento",
-      "Ratatouille",
-      "The Shawshank Redemption",
-      "Stardust",
-      "Valkyrie",
+      'Beetlejuice',
+      'City of God',
+      'The Cotton Club',
+      'Crocodile Dundee',
+      'The Intouchables',
+      'Memento',
+      'Ratatouille',
+      'The Shawshank Redemption',
+      'Stardust',
+      'Valkyrie',
     ]);
   });
 });
