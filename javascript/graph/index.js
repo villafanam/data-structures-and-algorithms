4d5e9e8180b63b9d7f0bafbd0c3919788b47472a
Marco <pastry07@gmail.com>
2023-04-03 00:44:46 -0700

    added test

--- javascript/graph/index.js
+++ javascript/graph/index.js
@@ -1,14 +1,14 @@
 'use strict';
 
-class Vertex {
+class Node {
   constructor(value) {
     this.value = value;
   }
 }
 
 class Edge {
-  constructor(vertex, weight = 0) {
-    this.vertex = vertex;
+  constructor(node, weight = 0) {
+    this.vertex = node;
     this.weight = weight;
   }
 }
@@ -18,19 +18,19 @@ class Graph {
     this.adjacencyList = new Map();
   }
 
-  addVertex(value) {
-    const vertex = new Vertex(value);
+  addNode(value) {
+    const vertex = new Node(value);
     this.adjacencyList.set(vertex, []);
     return vertex;
   }
 
-  addDirectedEdge(startVertex, endVertex, weight = 0) {
-    const neighbor = this.adjacencyList.get(startVertex);
-    neighbor.push(new Edge(endVertex, weight));
+  addDirectedEdge(startNode, endNode, weight = 0) {
+    const neighbor = this.adjacencyList.get(startNode);
+    neighbor.push(new Edge(endNode, weight));
   }
   // addEdge(vertex1, vertex2, weight = 0) { }
 
-  getVertex() {
+  getNodes() {
     let iterator = this.adjacencyList.keys();
     return iterator;
   }
@@ -59,8 +59,8 @@ class Graph {
 
       const neighbors = this.getNeighbors(current);
 
-      for(let edge of neighbors){
-        if(!visted.has(edge.vertex)){
+      for (let edge of neighbors) {
+        if (!visted.has(edge.vertex)) {
           visted.add(edge.vertex);
           queue.unshift(edge.vertex);
         }
@@ -68,29 +68,26 @@ class Graph {
     }
     return visted;
   }
- 
 
-  depthFirst(root, callback){
+
+  depthFirst(root, callback) {
     const stack = [root];
     const visited = new Set();
     visited.add(root);
     let current = null;
 
-    while(stack.length){
-      // the first time, we pop the vertex
+    while (stack.length) {
       current = stack.pop();
 
-      // if a callback exists, we use it to "do the thing"
       if (callback) {
         callback(current.value)
       }
 
-      // grab neighbor if it exists
       const neighbors = this.getNeighbors(current);
-      for (let edge of neighbors){
-        if (!visited.has(edge.vertex)){
+      for (let edge of neighbors) {
+        if (!visited.has(edge.vertex)) {
           visited.add(edge.vertex);
-          stack.push(edge.vertex)
+          stack.push(edge.vertex);
         }
       }
     }
@@ -98,36 +95,6 @@ class Graph {
   }
 
 }
-
-}
-
-const graph = new Graph();
-
-const A = graph.addVertex('A');
-const B = graph.addVertex('B');
-const C = graph.addVertex('C');
-const D = graph.addVertex('D');
-const E = graph.addVertex('E');
-const F = graph.addVertex('F');
-const G = graph.addVertex('G');
-const H = graph.addVertex('H');
-
-graph.addDirectedEdge(A, B);
-graph.addDirectedEdge(A, D);
-graph.addDirectedEdge(A, C);
-graph.addDirectedEdge(B, G);
-graph.addDirectedEdge(D, F);
-graph.addDirectedEdge(D, H);
-graph.addDirectedEdge(F, H);
-graph.addDirectedEdge(C, H);
-graph.addDirectedEdge(F, E);
-
-graph.breadthFirst(A, console.log);
-console.log('-------------------');
-graph.depthFirst(A, console.log)
-console.log('-------------------');
-console.log(graph.size());
-
 
 
 module.exports = Graph;
